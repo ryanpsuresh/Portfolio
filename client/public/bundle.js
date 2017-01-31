1ce68124471341af7c040255edc0a85ad664bb71
Ryan <ryanpsuresh@gmail.com>
2017-01-31 01:27:14 -0600

    center text on homepage

--- client/public/bundle.js
+++ client/public/bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "9a347ef0ca3aa49c0974"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "37306ca262eb1f6d62ac"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotMainModule = true; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
