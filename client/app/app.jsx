e980705249641f29e06612d995ad5c6a8f79896f
Ryan <ryanpsuresh@gmail.com>
2017-01-31 14:25:50 -0600

    position jumbotron

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -7,26 +7,17 @@ const App = () => (
     <Video autoPlay muted loop>
       <source src={mp4} type="video/mp4"/> Your browser does not support the video tag. 
     </Video>
-    <div class="container valign-wrapper" id="header">
-      <div class="valign center-align">
-        <div >
-          <h1>Ryan Suresh</h1>
-        </div>
-        <div >
-          <h3>Software Engineer</h3> 
-        </div>
-        <div >
-          <div class="icons">
-            <ul>
-              <li><a href="https://github.com/ryanpsuresh" target="_blank"><i class="fa fa-github fa-2x"></i></a></li>
-              <li><a href="https://linkedin.com/in/ryansuresh" target="_blank"><i class="fa fa-linkedin fa-2x"></i></a></li>
-              <li><a class="tooltipped" data-position="bottom" data-delay="50" data-tooltip="ricochenx@gmail.com"><i class="fa fa-envelope-o fa-2x"></i></a></li>
-            </ul>
+      <div className="container valign-wrapper" id="header">
+        <div className="jumbotron">
+          <div className="col s12">
+            <h1>Ryan Suresh</h1></div>
+          <div className="col s12">
+            <h3>Software Engineer</h3> 
           </div>
         </div>
-      </div>
     </div>
   </div>
+
 );
 
 export default App;
