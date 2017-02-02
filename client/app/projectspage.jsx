ac797c5352f386712c387313cd55eb4195c62edc
Ryan <ryanpsuresh@gmail.com>
2017-02-01 20:40:20 -0600

    add second jumbotron to portfolio page

--- client/app/projectspage.jsx
+++ client/app/projectspage.jsx
@@ -23,18 +23,28 @@ const Projectspage = () => (
           <div className='col-xs-6 col-sm-6 col-md-6 col-lg-6 col-centered'>
             <div className='jumbotron portfolioFrame'>
               <div className="card stack">
-              <h4 className="card-title">Travel App</h4>
-              <img className="card-img-top" src={travelAppImage} alt="Card image cap"/>
-              <div className="card-block">
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h4 className="card-title">Travel App</h4>
+                <img className="card-img-top" src={travelAppImage} alt="Card image cap"/>
+                <div className="card-block">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
             </div>
           </div>
         </div>
-          </div>
-          <div className='col-xs-6 col-sm-6 col-md-6 col-lg-6 col-centered'>
+        <div className='col-xs-6 col-sm-6 col-md-6 col-lg-6 col-centered'>
+          <div className='jumbotron portfolioFrame'>
+            <div className="card stack">
+            <h4 className="card-title">Travel App</h4>
+            <img className="card-img-top" src={travelAppImage} alt="Card image cap"/>
+            <div className="card-block">
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
           </div>
         </div>
+        </div>
+      </div>
       </div>
     </div>
 
